92231eea99cc4e33feb07592e74a45867813f0b3
hp <yaohuahp123@gmail.com>
2016-11-22 13:44:43 +0800

    nothing

--- filter.cpp
+++ filter.cpp
@@ -1,33 +1,107 @@
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include<iomanip>
 using namespace std;
-#define MAXN = 100000000;
-//all use a temporary tow-dimensional array
-int data[MAXN][MAXN];//the whole data without the first line
+
 //row 15:SUM_YR_1,row16:SUM_YR_2,29:AVG_DISCOUNT,17:SEG_KM_SUM
-int filter_data(datafile){
-	int index_15 = datafile[15];
-	int index_16 = datafile[16],
-	int index_17 = datafile[17],
-	int index_29 = datafile[29],
-	int filterflag = 1;
-	if(!isdigit(index_15)||!isdigit(index_16)) filterflag = 0;
-	if(index_15==0&&index_16==0)
-		if(index_17>0&&index_29!=0)
-		filterflag = 0;
-	return filterflag;
+int split(const string &src,const char split_char,vector<string>&res){
+    //split string by split_char
+    int len = src.size();
+    string one_split="";
+    for(int i=0;i<len;i++){
+        if(src[i]==split_char){
+            res.push_back(one_split);
+            one_split="";
+        }else{
+            one_split+=src[i];
+        }
+    }
+    //the last part
+    if(one_split.size()!=0){
+        res.push_back(one_split);
+    }
+    return res.size();
+}
+
+bool isNum(const string &str){
+    // string is number or not
+    stringstream sin(str);
+    double d;
+    char c;
+    if(!(sin >> d)){
+         return false;
+    }
+     if (sin >> c){
+       return false;
+     }
+     return true;
+}
+vector<int> clean_line;//store the number of cleaned lines
+void filter(){
+	fstream f("hw1air_data.csv");
+	string line;
+	//attribute line
+	getline(f,line,'\n');
+
+	vector<string> attribute_line;
+	int attribute_num=split(line,',',attribute_line);
+
+	vector<string> data_line;
+	int total = 0;
+	float value = 0;
+	while (f.good())
+	{
+		total++;//number of data_line
+		data_line.clear();
+
+		getline(f,line,'\n');
+
+		//split the data_line
+		split(line,',',data_line);
+
+		//attribute_number!=44 error lines
+		if(data_line.size()!=attribute_num){
+			clean_line.push_back(total);
+			continue;
+		}
+
+		float YR_1 = atof(data_line[15].data());
+		float YR_2 = atof(data_line[16].data());
+		float DISCOUNT = atof(data_line[29].data());
+		float SEG = atof(data_line[17].data());
+
+		if((YR_1==0)&&(YR_2==0)){
+			if(DISCOUNT&&!SEG){
+				clean_line.push_back(total);
+				continue;
+			}
+		}	
+	}
 }
 
 int main(){
-	int filterflag;
-	for(int i=0;i<end;i++)
-	{
-		filterflag = filter_data(data[i]);
-		if (filterflag==0)
-		{
-			for(int j=i;j<end;j++) data[j] = data[j+1];//delete line too complicated in delete ,consider the linked list
+	filter();
+	fstream f("hw1air_data.csv");
+	string line;
+	//once again
+	freopen("cleaned_out.csv","w",stdout);
+	getline(f,line,'\n');
+
+	int curr=0;
+	int clean_num=clean_line.size();
+	int flag=0;
+	while(f.good()){
+		curr++;
+		getline(f,line,'\n');
+		for(int i=0;i<clean_num;i++){
+			if(curr == clean_line[i]) {flag = 1;break;}
 		}
+		if(flag) continue;
+		cout<<line<<endl;
 	}
-	freopen("filtered.dat","w",data);
 	return 0;
 }
+
